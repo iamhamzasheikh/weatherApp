1fb4456106fbe97be0f3f4b24886c23c07ffd7f9
Hamza Sheikh <zafarhamza789@gmail.com>
2024-11-17 12:22:03 +0500

    weatherApp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Weather from './Components/Weather'
+
+function App() {
+  return (
+    <>
+    <div className='app'>
+      <Weather/>
+    </div>
+    </>
+  )
+}
+
+export default App
